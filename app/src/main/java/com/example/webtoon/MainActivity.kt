ffa62d27df6966566a75dab51f443328abeeec4d
강진우 <wlsdn1252@naver.com>
2023-04-19 03:41:06 +0900

    네이버웹툰 페이지를 보여줄 webview설정

--- app/src/main/java/com/example/webtoon/MainActivity.kt
+++ app/src/main/java/com/example/webtoon/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.webtoon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebViewClient
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        webView.webViewClient = WebViewClient() // 웹뷰 클라이언트 설정
+        webView.settings.javaScriptEnabled = true   // 웹뷰 안에서도 자바스크립트 사용 가능하도록 설정
+
+        webView.loadUrl("https://google.com")   //웹뷰에서 띄울 페이지의 URL연결
     }
 }
